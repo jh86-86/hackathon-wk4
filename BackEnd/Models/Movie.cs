7c5f59a699a94d7bac1ef93f596637ba643bf44d
Jenn <jenndoyle@hotmail.co.uk>
2021-02-07 09:51:12 +0000

    colors

--- BackEnd/Models/Movie.cs
+++
@@ -1,9 +0,0 @@
-
-public class Movie
-    {
-    public long? Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string Time { get; set; }
-
-    }
